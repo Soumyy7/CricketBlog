05e2117f5f325c2265b93eeac97e1d635582b3de
Soumyy7 <136830871+Soumyy7@users.noreply.github.com>
2023-07-25 02:02:43 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -14,7 +14,7 @@ const Create = () => {
 
     setIsPending(true);
 
-    fetch("https://blogjson-j9ts.onrender.com/blogs", {
+    fetch("https://blogjson-j9ts.onrender.com/blogs/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog),
